89eb337b8fd7581409c51dd5e949e8ab20be253c
Alexis <alexisvt@gmail.com>
2017-09-04 07:57:26 -0600

    adding more code

---
+++ graphql-server-sample/README.md
@@ -0,0 +1,3 @@
+# Another Sample of use on graphql-server package
+
+The idea of this sample is a use case of graphql-server package (apollo server) plus TypeScript
---
+++ graphql-server-sample/src/utils/index.ts
@@ -0,0 +1,22 @@
+import fs from "fs";
+import path from "path";
+
+export const loadSchemaFile = (schemaName = "", isQuery = true) => {
+  return new Promise((resolve, reject) => {
+
+    if (schemaName) {
+
+      const pathToResolve = path.resolve("src", "schemas", isQuery ? "types" : "", schemaName);
+
+      fs.readFile(pathToResolve, "utf8", (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data);
+      });
+    } else {
+      return reject("Invalid schemaName");
+    }
+  });
+
+};
