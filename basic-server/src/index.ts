6b1fa1112096d3011f590e9b0403668708eb651d
Alexis <alexisvt@gmail.com>
2017-09-05 21:20:07 -0600

    adding changes

--- basic-server/src/index.ts
+++ basic-server/src/index.ts
@@ -1,14 +1,25 @@
+import { GraphQLOptions } from 'apollo-server-core';
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
-import { graphiqlExpress } from 'graphql-server-express';
+import {
+  graphiqlExpress,
+  graphqlExpress,
+} from 'graphql-server-express';
+import { loadSchemaFile } from './utils';
 
 const app = express();
 const PORT = 5687;
+
+const GraphQLOptions: GraphQLOptions = {
+  schema
+};
+
+app.use('/graphql', bodyParser.json(), graphqlExpress(GraphQLOptions));
 
 app.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
 }));
 
 app.listen(PORT, () => {
-  console.log(`listening port: ${PORT}`);
+  console.log(`listening port: http://locahost:${PORT}`);
 });
